356d5426f1c281f1795a21e0726684f1741df12f
cy19900702 <cy19900702@sina.cn>
2016-09-09 14:06:24 +0800

    资产ListView新增按钮 批量传递勾选的资产行信息

--- modules/HAT_Assets/views/view.list.php
+++ modules/HAT_Assets/views/view.list.php
@@ -7,25 +7,31 @@ class HAT_AssetsViewList extends ViewList
     function Display() {
         global $app_list_strings;
         $this->ss->assign('APP_LIST', $app_list_strings);
-
-        parent::Display();
+        
+		parent::Display();
+    }
+	
+	public function preDisplay(){
+        parent::preDisplay();
+        //$this->lv->actionsMenuExtraItems[] = $this->getNewActionMenuItem();
+        $this->lv->actionsMenuExtraItems[] = $this->buildMyMenuItem();
+    }
+	
+	
+protected function buildMyMenuItem()
+    {
+        global $app_strings;
+        return <<<EOHTML
+<a class="menuItem" style="width:150px;" href="#" onmouseover='hiliteItem(this,"yes");'
+        onmouseout='unhiliteItem(this);'
+        onclick="sugarListView.get_checks();
+        if(sugarListView.get_checks_count() &lt; 1) {
+            alert('{$app_strings['LBL_LISTVIEW_NO_SELECTED']}');
+            return false;
+        }
+        document.MassUpdate.action.value='displaypassedids';
+        document.MassUpdate.submit();">传递资产到EBS</a>
+EOHTML;
     }
 
-/*    function listViewProcess(){
-
-
-		$c = BeanFactory::getBean('HAT_Assets',$this->bean->id);
-		   //because that field is not included in my list view. By using the $bean id to retrieve a copy of the full bean (which I called $c)
-		   //Ref:http://developer.sugarcrm.com/2012/10/15/conditional-formatting-on-cases-list-view-and-dashlets/
-		$val = translate($c->field_defs['asset_status']['options'],'', $c->asset_status);
-		//$this->bean->asset_status_displayed = "<span class='color_tag color_asset_status_{$c->asset_status}'>tag:{$val}</span>";
-		
-		$this->bean->asset_status = "test";//"<span class='color_tag color_asset_status_{$c->asset_status}'>tag:{$val}</span>";
-
-		/*foreach($this->bean as $key => $value) {
-           print "$key => $value\n";
-       }
-
-        parent::listViewProcess();
-    }*/
 }
