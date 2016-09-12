87e4adf510f5bb61daca1d68bb2f18dd9211ce27
cy19900702 <cy19900702@sina.cn>
2016-09-12 11:23:24 +0800

    Revert "Revert "sync 2016-09-12""
    This reverts commit 972ba15de3f9ac7aff53bdb4a13a963332af4acb.

--- modules/HIT_IP_TRANS_BATCH/js/hit_ip_trans_batch_editview.js
+++ modules/HIT_IP_TRANS_BATCH/js/hit_ip_trans_batch_editview.js
@@ -1,3 +1,15 @@
+function setWoPopupReturn(popupReplyData){
+	set_return(popupReplyData);
+	if($("#source_wo").val()=="") {
+		$("#source_woop").val("");
+		$("#source_woop_id").val("");
+		mark_field_disabled("source_woop",false);
+	}else{
+		mark_field_enabled("source_woop",true);
+	}
+}
+
+
 function setEventTypePopupReturn(popupReplyData){
 	set_return(popupReplyData);
 	setEventTypeFields();
@@ -24,8 +36,108 @@ function setEventTypeFields() {
 function resetEventType(){
 };
 
+/**
+ * 设置必输
+ */
+function mark_field_enabled(field_name,not_required_bool) {
+  //field_name = 字段名，不需要jquery select标志，直接写名字
+  //not_required_bool如果为空或没有明确定义为true的话，字段为必须输入。如果=true则为非必须
+  //alert(not_required_bool);
+  $("#"+field_name).css({"color":"#000000","background-Color":"#ffffff"});
+  $("#"+field_name).attr("readonly",false);
+  $("#"+field_name+"_label").css({"color":"#000000","text-decoration":"none"})
+
+  if(typeof not_required_bool == "undefined" || not_required_bool==false || not_required_bool=="") {
+      addToValidate('EditView', field_name,'varchar', 'true', $("#"+field_name+"_label").text());//将当前字段标记为必须验证
+      //打上必须星标
+      if  ($("#"+field_name+"_label .required").text()!='*') {//如果没有星标，则打上星标
+        $("#"+field_name+"_label").html($("#"+field_name+"_label").text()+"<span class='required'>*</span>");//打上星标
+      } else {//如果已经有星标了，则显示出来
+        $("#"+field_name+"_label .required").show();
+      }
+  } else { //如果不是必须的，则不显示星标
+    //直接Remove有时会出错，所有先设置为Validate再Remove
+    addToValidate('EditView', field_name,'varchar', 'true', $("#"+field_name+"_label").text());
+    removeFromValidate('EditView',field_name);
+     //去除必须验证
+    $("#"+field_name+"_label .required").hide();
+  }
+  if  (typeof $("#btn_"+field_name)!= 'undefined') {//移除选择按钮
+    $("#btn_"+field_name).css({"visibility":"visible"});
+  }
+  if  (typeof $("#btn_clr_"+field_name)!= 'undefined') {//移除清空按钮
+    $("#btn_clr_"+field_name).css({"visibility":"visible"});
+  }
+}
+//设置字段不可更新
+function mark_field_disabled(field_name, hide_bool, keep_position=false) {
+	  mark_obj = $("#"+field_name);
+	  mark_obj_lable = $("#"+field_name+"_label");
+
+	  if(hide_bool==true) {
+	  	if (keep_position==false) {
+	    	mark_obj.closest('td').css({"display":"none"});
+	    	mark_obj_lable.css({"display":"none"});
+		}else{
+	    	mark_obj.closest('td').css({"display":"table-column"});
+	    	mark_obj_lable.css({"display":"table-column"});
+		}
+	  }else{
+	  	mark_obj.closest('td').css({"display":""});
+	    mark_obj_lable.css({"display":""});
+		mark_obj.css({"color":"inherit","background-Color":"#efefef;"});
+	  	mark_obj.attr("readonly",true);
+	  	mark_obj_lable.css({"color":"#aaaaaa"});
+	  }
+	  if (typeof validate != "undefined" && typeof validate['EditView'] != "undefined") {
+	    removeFromValidate('EditView',field_name); // 去除必须验证
+	  }
+	  $("#"+field_name+"_label .required").hide();
+
+	  if  (typeof $("#btn_"+field_name)!= 'undefined') {
+	    $("#btn_"+field_name).css({"visibility":"hidden"});
+	  }
+	  if  (typeof $("#btn_clr_"+field_name)!= 'undefined') {
+	    $("#btn_clr_"+field_name).css({"visibility":"hidden"});
+	  }
+	}
+
 $(document).ready(function(){
-
-	//...
-
-});
+	mark_field_disabled("email",false);
+	if($("#source_wo").val()=="") {
+		mark_field_disabled("source_woop",false);
+	}
+	
+	$("#source_wo").change(function(){
+		if($("#source_wo").val()=="") {
+			$("#source_woop").val("");
+			$("#source_woop_id").val("");
+			mark_field_disabled("source_woop",false);
+		}
+	});	
+	
+	$("#btn_clr_source_wo").click(function(){
+		if($("#source_wo").val()=="") {
+			$("#source_woop").val("");
+			$("#source_woop_id").val("");
+			mark_field_disabled("source_woop",false);
+		}
+	});	
+	
+	$("#contact_name").change(function(){
+		if($("#contact_name").val()=="") {
+			$("#email").val("");
+			mark_field_disabled("source_woop",false);
+		}
+	});	
+	
+	$("#btn_clr_contact_name").click(function(){
+		if($("#contact_name").val()=="") {
+			$("#email").val("");
+			mark_field_disabled("email",false);
+		}
+	});	
+	
+	
+}
+)
