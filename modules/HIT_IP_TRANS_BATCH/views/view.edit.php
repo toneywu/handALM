09d3b39635ee80d72e3e283c41a8aac98ba91c0a
cy19900702 <cy19900702@sina.cn>
2016-09-12 11:25:56 +0800

    ddd

--- modules/HIT_IP_TRANS_BATCH/views/view.edit.php
+++ modules/HIT_IP_TRANS_BATCH/views/view.edit.php
@@ -1,22 +1,4 @@
 <?php
-if (!defined('sugarEntry') || !sugarEntry)
-	die('Not A Valid Entry Point');
-
-require_once ('include/MVC/View/views/view.edit.php');
-
-class HIT_IP_TRANS_BATCHViewEdit extends ViewEdit {
-
-	function Display() {
-
-		global $current_user;
-		global $db;
-		
-
-		parent :: Display();
-	}
-
-<<<<<<< HEAD  
->>>>>>> lab3
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 require_once('include/MVC/View/views/view.detail.php');
 class HIT_IP_TRANS_BATCHViewEdit extends ViewEdit
