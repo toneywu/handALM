ee872ec54422994ce48e1c7e81bb4785a27d8f41
cy19900702 <cy19900702@sina.cn>
2016-10-10 11:25:28 +0800

    commit 2016-10-10- 11:27

--- modules/HAT_Assets/js/detailview_HAT_Assets.js
+++ modules/HAT_Assets/js/detailview_HAT_Assets.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+	if($('#haa_ff_id').length==0) {//如果对象不存在就添加一个
+		$("#EditView").append('<input id="haa_ff_id" name="haa_ff_id" type=hidden>');
+	};
 
+    //触发FF
+    SUGAR.util.doWhen("typeof setFF == 'function'", function(){
+        triger_setFF($("#haa_ff_id").val(),"HAT_Assets")
+		$(".expandLink").click();
+     });
+     
 	$("#asset_desc").css("font-weight","bold");
 	$("#name").css("font-weight","bold");
 
