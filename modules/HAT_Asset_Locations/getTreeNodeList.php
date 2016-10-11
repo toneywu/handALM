3a8630208324e839d5863018c5cba8ee2908c0a6
toneywu <toney_wu@hotmail.com>
2016-10-11 14:06:03 +0800

    引入当前工单/网络资源事务

--- modules/HAT_Asset_Locations/getTreeNodeList.php
+++ modules/HAT_Asset_Locations/getTreeNodeList.php
@@ -34,7 +34,45 @@ if ($_REQUEST['type']=="wo_asset_trans" && isset($_REQUEST['wo_id'])) {
                           AND hat_asset_trans_batch.`asset_trans_status` != 'CANCELED'
                           AND ".$current_mode_sql. " ORDER by hat_assets.name ASC";
                          //
-        //echo $sel_sub_asset;
 }
+
+if ($_REQUEST['type']=="wo_ip_trans" && isset($_REQUEST['wo_id'])) {
+    //wo_ip_trans 显示当前工作单的所有网络资源事务处理行中出现的内容
+
+    //因为存在多个资产事务处理行处理了同一个资产的情况（比如不同资产事务单），因此在结果中需要DISTINCT
+        $sel_sub_asset ="SELECT DISTINCT 
+                          hat_assets.id, hat_assets.name, hat_assets.asset_desc, hat_assets.asset_icon, hat_assets.asset_status 
+                        FROM
+                          hat_assets 
+                        WHERE hat_assets.`deleted` = 0 
+                          AND hat_assets.id IN 
+                          (SELECT 
+                            hit_ip_trans.hat_assets_id 
+                          FROM
+                            hit_ip_trans,
+                            hit_ip_trans_batch 
+                          WHERE hit_ip_trans.`deleted` = 0 
+                            AND hit_ip_trans_batch.`deleted` = 0 
+                            AND hit_ip_trans.`hit_ip_trans_batch_id` = hit_ip_trans_batch.`id` 
+                            AND hit_ip_trans_batch.`asset_trans_status` != 'DRAFT' 
+                            AND hit_ip_trans_batch.`asset_trans_status` != 'CANCELED'
+                            AND hit_ip_trans_batch.`source_wo_id`='".$_REQUEST['wo_id']."'
+                          UNION ALL SELECT 
+                            hit_ip_trans.access_assets_id 
+                          FROM
+                            hit_ip_trans,
+                            hit_ip_trans_batch 
+                          WHERE hit_ip_trans.`deleted` = 0 
+                            AND hit_ip_trans_batch.`deleted` = 0 
+                            AND hit_ip_trans.`hit_ip_trans_batch_id` = hit_ip_trans_batch.`id` 
+                            AND hit_ip_trans_batch.`asset_trans_status` != 'DRAFT' 
+                            AND hit_ip_trans_batch.`asset_trans_status` != 'CANCELED' 
+                            AND hit_ip_trans_batch.`source_wo_id`='".$_REQUEST['wo_id']."'
+                            )
+                            AND ".$current_mode_sql. " ORDER by hat_assets.name ASC";
+}
+
+//echo $sel_sub_asset;
+
 ?>
 
