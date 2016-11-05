466e4085704ec12281cbe272a2ffda8ddecec2c3
cy19900702 <cy19900702@sina.cn>
2016-11-05 23:31:09 +0800

    commit on tonewu/master

--- modules/HIT_IP_TRANS_BATCH/getEventJsonData.php
+++ modules/HIT_IP_TRANS_BATCH/getEventJsonData.php
@@ -30,7 +30,7 @@ $event_sql = "SELECT
 			  ,h.change_monitoring_backup
 			  ,h.change_channel_content_backup
 			  ,h.change_channel_num_backup
-			  ,h.change_status,h.change_access_assets_backup_name,h.change_enable_action,h.need_limit_transaction_lines
+			  ,h.change_status,h.change_access_assets_backup_name,h.change_enable_action
 			FROM
 			  hat_eventtype h 
 			WHERE h.deleted = 0 
