7b178cfa690eacaf9995ef22def0d2f17419e6e3
osmondliu <lq5495@163.com>
2016-11-07 15:14:51 +0800

    Contracts/Insurance list

--- custom/modules/AOS_Contracts/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/AOS_Contracts/Ext/Vardefs/vardefs.ext.php
@@ -14,6 +14,12 @@ $dictionary['AOS_Contracts']['fields']['contract_number_c']['labelValue']='Contr
 
  
 
+ // created: 2016-10-21 12:12:11
+$dictionary['AOS_Contracts']['fields']['contract_revision_c']['inline_edit']='1';
+$dictionary['AOS_Contracts']['fields']['contract_revision_c']['labelValue']='版本号';
+
+ 
+
  // created: 2016-06-18 11:50:17
 $dictionary['AOS_Contracts']['fields']['haa_codes_id1_c']['inline_edit']=1;
 
