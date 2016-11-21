e06c39d8794d48fedbf6150fceafe4b10f664ad9
osmondliu <lq5495@163.com>
2016-11-21 11:50:37 +0800

    版本更新

--- modules/HPR_Group_Members/js/HPR_Group_Members.js
+++
@@ -1,8 +0,0 @@
-function setDefaultName(popupReplyData){
-	set_return(popupReplyData);//标准Popup-Return函数
-	var members_name=$("#organization").val();
-	if ($("#user_name").val()) {
-		var members_name=$("#user_name").val();
-	}
-	$("input[id='name']").val(members_name);
-}
--- modules/HPR_Group_Priviliges/js/HPR_Group_Priviliges.js
+++
@@ -1,9 +0,0 @@
-function setDefaultName(popupReplyData){
-	set_return(popupReplyData);//标准Popup-Return函数
-
-	var members_name=$("#privilige_module").val();
-	if ($("#group_member").val()) {
-		var members_name=$("#group_member").val();
-	}
-	$("input[id='name']").val(members_name);
-}
