ef0ecf3f7db93d64380e177e56fa027fc81a7d2c
cy19900702 <cy19900702@sina.cn>
2016-12-14 00:20:29 +0800

    FIX 2016-12-14

--- custom/Extension/modules/Schedulers/Ext/Language/en_us.custom_job.php
+++ custom/Extension/modules/Schedulers/Ext/Language/en_us.custom_job.php
@@ -1,10 +1,6 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
+<?php
 
-
-
-$mod_strings['LBL_CREATEREVENUESFROMCONTRACT']='Create Revenues From Contracts';
-$mod_strings['LBL_CUSTOM_JOB'] = 'Custom Job';
+    $mod_strings['LBL_CUSTOM_JOB'] = 'Custom Job';
 	$mod_strings['LBL_SYNC_JT_ACCOUNTS'] = '同步集团客户';
 	$mod_strings['LBL_SYNC_JT_PRODUCTS'] = '同步集团产品';
 	$mod_strings['LBL_SYNC_JT_CONTRACTS'] = '同步集团合同';
@@ -13,5 +9,4 @@ $mod_strings['LBL_CUSTOM_JOB'] = 'Custom Job';
 	$mod_strings['LBL_SYNC_XR_PRODUCTS'] = '同步欣润产品';
 	$mod_strings['LBL_SYNC_XR_CONTRACTS'] = '同步欣润合同';
 	$mod_strings['LBL_SYNC_XR_PO_INFOS'] = '同步欣润采购信息';
-
 ?>
