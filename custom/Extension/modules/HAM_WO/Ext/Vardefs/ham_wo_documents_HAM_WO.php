69923c9474f8bb37741a40da805e66721fe50f36
osmondliu <lq5495@163.com>
2016-12-30 23:43:25 +0800

    工单增加文档

---
+++ custom/Extension/modules/HAM_WO/Ext/Vardefs/ham_wo_documents_HAM_WO.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2016-12-30 23:34:15
+$dictionary["HAM_WO"]["fields"]["ham_wo_documents"] = array (
+  'name' => 'ham_wo_documents',
+  'type' => 'link',
+  'relationship' => 'ham_wo_documents',
+  'source' => 'non-db',
+  'module' => 'Documents',
+  'bean_name' => 'Document',
+  'vname' => 'LBL_HAM_WO_DOCUMENTS_FROM_DOCUMENTS_TITLE',
+);
