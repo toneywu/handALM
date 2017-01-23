6bf47d470cb3451c7b184d8509e9eebb2ccf0f53
osmondliu <lq5495@163.com>
2017-01-23 14:29:45 +0800

    通用数据导入

--- custom/application/Ext/Include/modules.ext.php
+++ custom/application/Ext/Include/modules.ext.php
@@ -52,6 +52,18 @@ $moduleList[] = 'HAA_FSQL';
 
  
  //WARNING: The contents of this file are auto-generated
+$beanList['HAA_Import_Datas'] = 'HAA_Import_Datas';
+$beanFiles['HAA_Import_Datas'] = 'modules/HAA_Import_Datas/HAA_Import_Datas.php';
+$modules_exempt_from_availability_check['HAA_Import_Datas'] = 'HAA_Import_Datas';
+$report_include_modules['HAA_Import_Datas'] = 'HAA_Import_Datas';
+$modInvisList[] = 'HAA_Import_Datas';
+$beanList['HAA_Import_Sets'] = 'HAA_Import_Sets';
+$beanFiles['HAA_Import_Sets'] = 'modules/HAA_Import_Sets/HAA_Import_Sets.php';
+$moduleList[] = 'HAA_Import_Sets';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 $beanList['HAA_Interfaces'] = 'HAA_Interfaces';
 $beanFiles['HAA_Interfaces'] = 'modules/HAA_Interfaces/HAA_Interfaces.php';
 $moduleList[] = 'HAA_Interfaces';
